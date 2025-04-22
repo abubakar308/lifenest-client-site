7ec787443c20771fe74e18b204658141877eafc0
abubakar308 <mdabubakarsiddique789@gmail.com>
2025-04-22 19:08:38 +0600

    login and register page updated

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,32 +1,31 @@
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content">
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
+   const {signIn} = useContext(AuthContext)
+       const navigate = useNavigate()
+       const {register, handleSubmit} = useForm()
+       const onSubmit = data => {
+        signIn(data?.email, data?.password)
+           if(data?.email){
+               navigate("/")
+           }
+       };
+       return (
+       <div className="w-full my-10 max-w-sm mx-auto text-center">
+           <h2 className="text-2xl py-4">LifeNest</h2>
+         <form onSubmit={handleSubmit(onSubmit)}>
+             <input type="email" placeholder="Email Address" className="input input-bordered w-full my-4" {...register("email")} required />
+   
+             <input type="password" placeholder="Password" className="input input-bordered w-full my-4" {...register("password")} required />
+        
+             <button type="submit" className="btn w-full btn-primary">Login</button>
+         </form>
+   
+      
+       </div>
     );
 };
 
