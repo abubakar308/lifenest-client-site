5b37444d5bc209caddb7c61514db98a6b79d0805
abubakar308 <mdabubakarsiddique789@gmail.com>
2025-04-24 00:06:47 +0600

    feat: salah data fetch in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,27 @@
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../provider/AuthProvider";
 
 
 const Profile = () => {
+
+    const {user} = useContext(AuthContext);
+
+    const [salahData, setSalahData] = useState([]);
+    console.log(salahData)
+
+    useEffect(()=>{
+        fetch(`http://localhost:3000/salah-data?email=${encodeURIComponent(user?.email)}`)
+        .then(res=>res.json())
+        .then(result=>{
+            console.log(result)
+            setSalahData(result)
+        })
+    },[user,setSalahData]);
+
     return (
         <div className="text-2xl">
-            profile
+           
+           <p>profile</p>
         </div>
     );
 };
